d38a67ec5fdec0d50d553f5a6aaf22f7d4f3038c
adaydan <66020039+adaydan@users.noreply.github.com>
2024-10-16 18:13:56 -0700

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,5 +1,34 @@
-from prefect import flow
+from prefect import flow, task
+import requests
+from playwright.sync_api import sync_playwright
+import pandas as pd
+global page
 
-@flow(log_prints=True)
-def my_flow():
-    print("I've made my own repo now :)")
+
+# @task
+# def print_variable(page):
+#
+#     return x
+@flow(log_prints=False)
+def launch_browser():
+    with sync_playwright() as p:
+        # Launch Chrome (non-headless)
+        browser = p.chromium.launch(headless=True, channel="chrome")
+
+        # Open a new browser page
+        page = browser.new_page()
+
+        page.goto("https://webscraper.io/test-sites")
+        x = [i.text_content() for i in page.query_selector_all('.site-heading a')]
+        print(x)
+        print('_______________')
+        for i in x:
+            print(i)
+        print('_______________')
+
+        browser.close()
+
+@flow(log_prints=False)
+def run_script():
+    launch_browser()
+    return
