536d29ad37aa1d0fc55be7404eb9a0743c69612d
adaydan <66020039+adaydan@users.noreply.github.com>
2024-10-17 12:00:03 -0700

    Update flow.py

--- flow.py
+++ flow.py
@@ -5,25 +5,25 @@ global page
 
 @flow(log_prints=True)
 def launch_browser():
+    ShellOperation(
+        commands=[
+            "playwright install chrome"
+        ]
+    ).run()
     print("starting...")
-    try:
-        print("good")
-        #subprocess.run(["playwright", "install", "chrome"], check=False)
-    except:
-        print("error")
-    # with sync_playwright() as p:
-    #     # Launch Chrome (non-headless)
-    #     browser = p.chromium.launch(headless=True, channel="chrome")
+    with sync_playwright() as p:
+        # Launch Chrome (non-headless)
+        browser = p.chromium.launch(headless=True, channel="chrome")
 
-    #     # Open a new browser page
-    #     page = browser.new_page()
+        # Open a new browser page
+        page = browser.new_page()
 
-    #     page.goto("https://webscraper.io/test-sites")
-    #     x = [i.text_content() for i in page.query_selector_all('.site-heading a')]
-    #     print(x)
-    #     print('_______________')
-    #     for i in x:
-    #         print(i)
-    #     print('_______________')
+        page.goto("https://webscraper.io/test-sites")
+        x = [i.text_content() for i in page.query_selector_all('.site-heading a')]
+        print(x)
+        print('_______________')
+        for i in x:
+            print(i)
+        print('_______________')
 
-    #     browser.close()
+        browser.close()
